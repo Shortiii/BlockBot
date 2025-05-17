109b8e9de93750e824aa101a1cd873d75070dc60
Oluwatoyin <105533960+Shortiii@users.noreply.github.com>
2025-05-17 16:51:16 +0100

    responsiveness

--- src/components/Trading/Trading.jsx
+++ src/components/Trading/Trading.jsx
@@ -27,23 +27,23 @@ const Trading = () => {
           <img src={thunder} alt="" className="hover-image" />
           <h1>LIGHTNING FAST SWAPS</h1>
         </div>
-        <div className="group group2">
+        <div className="group group2 icon">
           <img src={stroke} alt="" />
           <h1>TRADING INSIGHTS</h1>
         </div>
-        <div className="group group3">
+        <div className="group group3 icon">
           <img src={crown} alt="" />
           <h1>THE SOCIAL LAYER</h1>
         </div>
-        <div className="group">
+        <div className="group icon">
           <img src={diamond} alt="" />
           <h1>USER-SPECIFIC TRADING DATA</h1>
         </div>
-        <div className="group group5">
+        <div className="group group5 icon">
           <img src={profile} alt="" />
           <h1>USER PROFILES</h1>
         </div>
-        <div className="group click">
+        <div className="group icon click">
           <img src={question} alt="" />
           <h1>[REDACTED]</h1>
         </div>
